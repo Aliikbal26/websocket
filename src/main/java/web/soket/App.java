4578b9a640835269994975bdba4fe98dcd81cb0c
Aliikbal26 <aliikbaljamphers26@gmail.com>
2024-09-01 09:48:56 +0700

    updated file main and index

--- src/main/java/web/soket/App.java
+++ src/main/java/web/soket/App.java
@@ -1,18 +1,76 @@
+package web.soket;
 
-import org.glassfish.tyrus.server.Server;
-import main.java.web.socket.WebSocketData;
+import org.java_websocket.server.WebSocketServer;
+import org.java_websocket.WebSocket;
+import org.java_websocket.handshake.ClientHandshake;
+import com.google.gson.Gson;
 
-public class App {
-    public static void main(String[] args) {
-        Server server = new Server("localhost", 8080, "/", null, WebSocketData.class);
-        try {
-            server.start();
-            System.out.println("Stop...");
-            System.in.read();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            server.stop();
+import java.net.InetSocketAddress;
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class App extends WebSocketServer {
+    private static final Gson gson = new Gson();
+    private static final Random random = new Random();
+
+    public App(int port) {
+        super(new InetSocketAddress(port));
+    }
+
+    @Override
+    public void onOpen(WebSocket conn, ClientHandshake handshake) {
+        System.out.println("New connection: " + conn.getRemoteSocketAddress());
+    }
+
+    @Override
+    public void onClose(WebSocket conn, int code, String reason, boolean remote) {
+        System.out.println("Closed connection: " + conn.getRemoteSocketAddress());
+    }
+
+    @Override
+    public void onMessage(WebSocket conn, String message) {
+    }
+
+    @Override
+    public void onError(WebSocket conn, Exception ex) {
+        ex.printStackTrace();
+    }
+
+    @Override
+    public void onStart() {
+        System.out.println("WebSocket port: " + this.getPort());
+
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                int data = random.nextInt(10000);
+                String jsonData = gson.toJson(new DataModel(data));
+                broadcast(jsonData); // Broadcast data
+            }
+        }, 0, 1000); // 1 second
+    }
+
+    // Data model for JSON
+    private static class DataModel {
+        private int value;
+
+        public DataModel(int value) {
+            this.value = value;
         }
+
+        public int getValue() {
+            return value;
+        }
+
+        public void setValue(int value) {
+            this.value = value;
+        }
+    }
+
+    public static void main(String[] args) {
+        App server = new App(8080);
+        server.start();
     }
 }
